fdc530fd423bd66a0ca701ea35c513a29dd5afbf
lkjrewqq <lkjrewqq@163.com>
2021-12-19 14:55:36 +0800

    aaa

---
+++ src/main/java/com/example/helllowrord/HelllowrordApplication.java
@@ -0,0 +1,13 @@
+package com.example.helllowrord;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelllowrordApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HelllowrordApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/helllowrord/HelllowrordApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.helllowrord;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelllowrordApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
